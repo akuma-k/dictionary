3ea492a79560eeaa9324c95582965c428ffb75a0
steven.sh <steven.sh@kakaocorp.com>
2019-05-25 22:55:17 +0900

    deploy 가능하도록 빌드 스크립트 수정

--- app/src/main/java/com/akuma/dictionaryapp/DictionaryActivity.java
+++ app/src/main/java/com/akuma/dictionaryapp/DictionaryActivity.java
@@ -3,12 +3,15 @@ package com.akuma.dictionaryapp;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import com.akuma.dictionary.GetMeaning;
 
-public class Activity extends AppCompatActivity {
+public class DictionaryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        GetMeaning meaning = new GetMeaning();
+        meaning.getMeaning("Hello");
     }
 }
